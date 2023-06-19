3feda52971a63414063f395830ec3d8383d90919
Rndmi <SireOfAll79@yandex.ru>
2023-06-19 20:54:19 +0300

    initial commit

---
+++ src/main/java/ru/stepanov/skypro/hwcalculator1906/HwCalculator1906Application.java
@@ -0,0 +1,13 @@
+package ru.stepanov.skypro.hwcalculator1906;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwCalculator1906Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwCalculator1906Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/stepanov/skypro/hwcalculator1906/HwCalculator1906ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stepanov.skypro.hwcalculator1906;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwCalculator1906ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
